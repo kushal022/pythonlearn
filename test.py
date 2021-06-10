5f1e6e14300db6a205c326ecaef8ff3ce92ff06d
kushal022 <kushal.learn22@gmail.com>
2021-06-10 21:31:11 +0530

    make a program to calculate total expenses Take input number of products

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-name= input("enter your name:")
+# make a program to calculate total expenses Take input number of products
+book = input("Enter book price:")
+notebook = input("Enter notebook price:")
+pen = input("Enter pen price:")
+pencil = input("Enter pencil price:")
+marker = input("Enter marker price:")
+
+book = int(book)
+notebook = int(notebook)
+pen = int(pen)
+pencil = int(pencil)
+marker = int(marker)
+
+TotalExpense = book+notebook+pen+pencil+marker
+print("Total Expense:",TotalExpense)
