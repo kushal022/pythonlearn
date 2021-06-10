cc5bf60c6d8033d23a554a657134094488d968d1
kushal022 <kushal.learn22@gmail.com>
2021-06-10 21:55:25 +0530

    make a program to monthly salary take input one day salary

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
-# make a program to calculate total expenses Take input number of products
-book = input("Enter book price:")
-notebook = input("Enter notebook price:")
-pen = input("Enter pen price:")
-pencil = input("Enter pencil price:")
-marker = input("Enter marker price:")
-
-book = int(book)
-notebook = int(notebook)
-pen = int(pen)
-pencil = int(pencil)
-marker = int(marker)
-
-TotalExpense = book+notebook+pen+pencil+marker
-print("Total Expense:",TotalExpense)
+# make a program to monthly salary take input one day salary
+oneDaySalary = input("Enter your one day salary:")
+oneDaySalary = int(oneDaySalary)
+monthlySalary = oneDaySalary*30
+print("Monthly Salary:",monthlySalary)
